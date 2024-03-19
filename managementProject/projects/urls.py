6472412932157fa409cafd066db934eed63895e4
Denis-Scavarziago <91191771+Denis-Scavarziago@users.noreply.github.com>
2024-03-19 17:48:01 +0000

    bugfix Projects model and integrate form submission example

--- managementProject/projects/urls.py
+++ managementProject/projects/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
-    path("projects", views.projects),
-    path("projects/project/<int:order>", views.project)
+    path('', views.index),
+    path('projects/', views.projects, name='projects'),
 ]
