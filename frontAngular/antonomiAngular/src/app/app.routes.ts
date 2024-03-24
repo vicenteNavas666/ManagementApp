1d3128aeafdb5c8e12119ef0300290e83186b4a8
vicenteNavas666 <135588377+vicenteNavas666@users.noreply.github.com>
2024-03-24 14:50:26 +0000

    Creation of not found component

--- frontAngular/antonomiAngular/src/app/app.routes.ts
+++ frontAngular/antonomiAngular/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
 
 export const routes: Routes = [
-{path: '', component: HomeComponent}
-
+{path: '', component: HomeComponent}, 
+{path: '**', component: NotFoundComponent},  
 
 ];
